dfa1f04533f5af67eba9f4bd1f145ebfe22f80eb
Kamil Pasternak <49869722+dredziasty@users.noreply.github.com>
2023-09-24 21:40:57 +0200

    Accept OPTIONS method (#3)

---
+++ internal/interfaces/http/rest/middleware/cors.go
@@ -0,0 +1,20 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CORS(ctx *gin.Context) {
+	ctx.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE")
+	ctx.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+	ctx.Writer.Header().Set("Content-Type", "application/json")
+
+	if ctx.Request.Method == http.MethodOptions {
+		ctx.AbortWithStatus(http.StatusOK)
+		return
+	}
+
+	ctx.Next()
+}
