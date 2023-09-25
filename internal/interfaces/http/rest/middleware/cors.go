2009473cee68ee4fd60f1cbe83d5c71da1da4539
Łukasz Kolber <69080157+psp515@users.noreply.github.com>
2023-09-25 17:07:31 +0200

    feat(cors problems)

--- internal/interfaces/http/rest/middleware/cors.go
+++ internal/interfaces/http/rest/middleware/cors.go
@@ -7,12 +7,14 @@ import (
 )
 
 func CORS(ctx *gin.Context) {
-	ctx.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE")
+	ctx.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS")
 	ctx.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 	ctx.Writer.Header().Set("Content-Type", "application/json")
+	ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+	ctx.Writer.Header().Set("Vary", "Origin")
 
 	if ctx.Request.Method == http.MethodOptions {
-		ctx.AbortWithStatus(http.StatusOK)
+		ctx.AbortWithStatus(http.StatusNoContent)
 		return
 	}
 
