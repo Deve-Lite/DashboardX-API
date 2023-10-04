057c1372938034b9ea9fbc83eb0b34b4036f980a
Kamil Pasternak <49869722+dredziasty@users.noreply.github.com>
2023-10-04 22:32:59 +0200

    Run tests on PR (#6)

--- scripts/d_test.ps1
+++ scripts/d_test.ps1
@@ -1,3 +1,3 @@
 # Runs integration tests in the Docker
 
-docker compose run app go test -v -timeout 30s ./internal/interfaces/http/rest/handler
+docker compose run --rm app go test -v -timeout 30s ./internal/interfaces/http/rest/handler
