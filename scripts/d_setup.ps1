3ad3de2c7e59057747806ed88d8d3cad0f91f22d
Kamil Pasternak <kamil.pasternak94@gmail.com>
2023-10-10 14:50:26 +0200

    chore: add prune flag to setup script

--- scripts/d_setup.ps1
+++ scripts/d_setup.ps1
@@ -1,7 +1,16 @@
 # Clean setup of the system, forces to delete all stored data!
 
+param (
+    [Parameter(HelpMessage="Force to prune docker system")]
+    [switch]$Prune = $false
+)
+
 docker compose rm -vfs
 docker rmi -f dashboardx-api-app
+
+if ($Prune) {
+    docker system prune --all --force
+} 
 
 docker compose build --no-cache
 
